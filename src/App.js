bad35778db20fa581af2f562dd745afc6ed0485d
martagrio <marta.igr90@gmail.com>
2019-11-05 14:01:19 +0100

    Add edit view

--- src/App.js
+++ src/App.js
@@ -12,7 +12,32 @@ class App extends React.Component {
   render() {
     return (
       <div className="app">
-				this will be the best mood calendar in the whole wooooorld
+				<header className="app__header">
+					<h1 className="header__title">Life Calendar</h1>
+				</header>
+				<main className="app__main">
+					<div className="edit__box">
+						<label htmlFor="date">choose your date:</label>
+						<input type="date" name="date" id="date" min="2019-01-01" max="2019-12-31"/>
+						<p className="text__mood">mood:</p>
+							<label htmlFor="happy"> :)
+								<input type="radio" name="mood" id="happy" value=":)"/>
+							</label>
+							<label htmlFor="sad"> :(
+							 <input type="radio" name="mood" id="sad" value=":("/>
+						 </label>
+						 <label htmlFor="message">Message</label>
+						 <input type="text" className="text__message" name="message" id="message"/>
+					</div>
+					<div className="calendar__box">
+						<div className="day__box">
+						ASCO DE VIDA, TETE
+						</div>
+					</div>
+				</main>
+				<footer className="app__footer">
+					<p className="footer__copy">© Copyright Life Calendar by Marta García</p>
+				</footer>
       </div>
     );
   }
